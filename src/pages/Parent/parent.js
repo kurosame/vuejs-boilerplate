88a5b7e889f2089a753e05713182885561d19487
kurosame <kurosame1227@gmail.com>
2017-11-05 16:05:11 +0900

    add name attribute

--- src/pages/Parent/parent.js
+++ src/pages/Parent/parent.js
@@ -3,6 +3,7 @@ import { ADD_VALUE } from "../../vuex/types";
 import Child from "../../components/Child/Child.vue";
 
 export default {
+  name: "Parent",
   methods: {
     ...mapActions({
       addValue: ADD_VALUE
