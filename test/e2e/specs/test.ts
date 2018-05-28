00907fc5a49f1ac81af33250e5900968b13345a8
kurosame <kurosame1227@gmail.com>
2018-05-28 18:27:59 +0900

    add: screenshots to e2e test

--- test/e2e/specs/test.ts
+++ test/e2e/specs/test.ts
@@ -1,3 +1,4 @@
+import path from 'path'
 import puppeteer, { Browser, Page } from 'puppeteer'
 
 let browser: Browser
@@ -16,6 +17,11 @@ afterAll(() => {
 test('Click the button.add-value, update the count', async () => {
   await page.click('.add-value')
 
+  await page.screenshot({
+    path: path.join(__dirname, '__screenshots__', 'add-value.png'),
+    fullPage: true
+  })
+
   expect(await page.$eval('.count', v => v.textContent)).toEqual('1')
 })
 
@@ -23,12 +29,22 @@ test('Click the button.axios-sample, update the axiosCount', async () => {
   await page.click('.axios-sample')
   await page.waitFor(1000)
 
+  await page.screenshot({
+    path: path.join(__dirname, '__screenshots__', 'axios-sample.png'),
+    fullPage: true
+  })
+
   expect(await page.$eval('.axios-count', v => v.textContent)).toEqual('2')
 })
 
 test('Click the button.async-await-sample, update the asyncAwaitCount', async () => {
   await page.click('.async-await-sample')
   await page.waitFor(1000)
+
+  await page.screenshot({
+    path: path.join(__dirname, '__screenshots__', 'async-await-sample.png'),
+    fullPage: true
+  })
 
   expect(await page.$eval('.async-await-count', v => v.textContent)).toEqual(
     '3'
