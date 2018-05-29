1d221bd89e3745b06a375dd944e226a9e7f9ab7c
kurosame <kurosame1227@gmail.com>
2018-05-29 12:02:56 +0900

    bugfix

--- test/e2e/specs/test.ts
+++ test/e2e/specs/test.ts
@@ -5,19 +5,22 @@ let browser: Browser
 let page: Page
 
 beforeAll(async () => {
-  browser = process.env.CI
-    ? await puppeteer.launch({
-        headless: true,
-        timeout: 0,
-        args: ['--no-sandbox']
-      })
-    : await puppeteer.launch({ headless: false, timeout: 0 })
+  browser =
+    process.env.CI === 'true'
+      ? await puppeteer.launch({
+          headless: true,
+          timeout: 0,
+          args: ['--no-sandbox']
+        })
+      : await puppeteer.launch({ headless: false, timeout: 0 })
   page = await browser.newPage()
-
-  await page.goto('http://localhost:9000')
 })
 afterAll(() => {
   browser.close()
+})
+
+beforeEach(async () => {
+  await page.goto('http://localhost:9000', { timeout: 10000 })
 })
 
 test('Click the button.add-value, update the count', async () => {
