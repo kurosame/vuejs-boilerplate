d445283deb44afe376eddb93da8aec7effc3e9e6
kurosame <kurosame1227@gmail.com>
2018-12-31 23:21:53 +0900

    fix: e2e test

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   server: {
     command: 'webpack-dev-server --port 9000 --mode development',
+    launchTimeout: 30000,
     port: 9000
   }
 }
--- test/e2e/specs/test.ts
+++ test/e2e/specs/test.ts
@@ -25,43 +25,53 @@ beforeEach(async () => {
   await page.goto('http://localhost:9000')
 })
 
+test('Initial display', async () => {
+  await page.screenshot({
+    path: path.join(__dirname, '__screenshots__', 'initial-display.png'),
+    fullPage: true
+  })
+})
+
 test('Click the add-count, update the count', async () => {
+  await page.waitForSelector('[data-test="add-count"]')
   await page.click('[data-test="add-count"]')
+
+  expect(await page.$eval('[data-test="count"]', v => v.textContent)).toEqual(
+    '1'
+  )
 
   await page.screenshot({
     path: path.join(__dirname, '__screenshots__', 'add-count.png'),
     fullPage: true
   })
-
-  expect(await page.$eval('[data-test="count"]', v => v.textContent)).toEqual(
-    '1'
-  )
 })
 
 test('Click the add-axios-count, update the axiosCount', async () => {
+  await page.waitForSelector('[data-test="add-axios-count"]')
   await page.click('[data-test="add-axios-count"]')
   await page.waitFor(1000)
+
+  expect(
+    await page.$eval('[data-test="axios-count"]', v => v.textContent)
+  ).toEqual('2')
 
   await page.screenshot({
     path: path.join(__dirname, '__screenshots__', 'add-axios-count.png'),
     fullPage: true
   })
-
-  expect(
-    await page.$eval('[data-test="axios-count"]', v => v.textContent)
-  ).toEqual('2')
 })
 
 test('Click the add-async-await-count, update the asyncAwaitCount', async () => {
+  await page.waitForSelector('[data-test="add-async-await-count"]')
   await page.click('[data-test="add-async-await-count"]')
   await page.waitFor(1000)
+
+  expect(
+    await page.$eval('[data-test="async-await-count"]', v => v.textContent)
+  ).toEqual('3')
 
   await page.screenshot({
     path: path.join(__dirname, '__screenshots__', 'add-async-await-count.png'),
     fullPage: true
   })
-
-  expect(
-    await page.$eval('[data-test="async-await-count"]', v => v.textContent)
-  ).toEqual('3')
 })
