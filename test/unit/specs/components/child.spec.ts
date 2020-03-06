58cf29051207f9d8b30e015573a312cf68c18204
kurosame <kurosame1227@gmail.com>
2020-03-06 21:21:44 +0900

    fix: unit test for pages to containers

--- test/unit/specs/components/child.spec.ts
+++ test/unit/specs/components/child.spec.ts
@@ -1,59 +1,19 @@
-import Vuex from 'vuex'
-import { mount, createLocalVue } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import Child from '@/components/Child.vue'
-import { ADD_COUNT, ADD_AXIOS_COUNT, ADD_ASYNC_AWAIT_COUNT } from '@/vuex/types'
-
-const localVue = createLocalVue()
-localVue.use(Vuex)
-
-const addCount = jest.fn().mockReturnValue(() => '')
-const addAxiosCount = jest.fn()
-const addAsyncAwaitCount = jest.fn()
-
-const store = new Vuex.Store({
-  actions: {
-    [ADD_COUNT]: addCount,
-    [ADD_AXIOS_COUNT]: addAxiosCount,
-    [ADD_ASYNC_AWAIT_COUNT]: addAsyncAwaitCount
-  },
-  getters: {
-    counter: () => ({ count: 147, axiosCount: 258, asyncAwaitCount: 369 })
-  }
-})
 
 const wrapper = mount(Child, {
-  localVue,
-  store
+  propsData: { count: 147 }
 })
+wrapper.vm.$emit('add-count')
 
-test('Data binding from store counter.count to the count', () => {
+test('Data binding props.count to count', () => {
   expect(wrapper.find('[data-test="count"]').text()).toEqual('147')
 })
 
-test('Click the add-count will emit the addCount', () => {
+test('Click the add-count will emit the add-count', () => {
   wrapper.find('[data-test="add-count"]').trigger('click')
 
-  expect(addCount).toBeCalled()
-})
-
-test('Data binding from store counter.axiosCount to the axiosCount', () => {
-  expect(wrapper.find('[data-test="axios-count"]').text()).toEqual('258')
-})
-
-test('Click the add-axios-count will emit the addAxiosCount', () => {
-  wrapper.find('[data-test="add-axios-count"]').trigger('click')
-
-  expect(addAxiosCount).toBeCalled()
-})
-
-test('Data binding from store counter.asyncAwaitCount to the asyncAwaitCount', () => {
-  expect(wrapper.find('[data-test="async-await-count"]').text()).toEqual('369')
-})
-
-test('Click the add-async-await-count will emit the addAsyncAwaitCount', () => {
-  wrapper.find('[data-test="add-async-await-count"]').trigger('click')
-
-  expect(addAsyncAwaitCount).toBeCalled()
+  expect(wrapper.emitted('add-count')).toBeTruthy()
 })
 
 test('Match the snapshot', () => {
--- test/unit/specs/pages/parent.spec.ts
+++ test/unit/specs/pages/parent.spec.ts
@@ -1,7 +1,22 @@
+import Vuex from 'vuex'
 import Parent from '@/containers/Parent.vue'
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import { CounterState } from '@/vuex/modules/counter'
 
-const wrapper = shallowMount(Parent)
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+const store = new Vuex.Store({
+  getters: {
+    counter: (): CounterState => ({
+      count: 147,
+      axiosCount: 258,
+      asyncAwaitCount: 369
+    })
+  }
+})
+
+const wrapper = shallowMount(Parent, { localVue, store })
 
 test('Match the snapshot', () => {
   expect(wrapper.html()).toMatchSnapshot()
