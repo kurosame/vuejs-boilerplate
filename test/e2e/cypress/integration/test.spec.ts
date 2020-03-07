40a9aa3b43bb41ccea88c1bafac19f1c66b371ff
kurosame <kurosame1227@gmail.com>
2020-03-07 15:12:44 +0900

    fix: e2e test for pages to containers

--- test/e2e/cypress/integration/test.spec.ts
+++ test/e2e/cypress/integration/test.spec.ts
@@ -1,4 +1,6 @@
+/* eslint-disable spaced-comment */
 /// <reference types="Cypress" />
+/* eslint-enable spaced-comment */
 
 context('Test', () => {
   beforeEach(() => {
@@ -6,39 +8,69 @@ context('Test', () => {
   })
 
   it('Initial display', () => {
-    cy.get('[data-test="count"]').should('have.text', '0')
-    cy.get('[data-test="axios-count"]').should('have.text', '0')
-    cy.get('[data-test="async-await-count"]').should('have.text', '0')
+    cy.get('[data-test="count"]')
+      .eq(0)
+      .should('have.text', '0')
+    cy.get('[data-test="count"]')
+      .eq(1)
+      .should('have.text', '0')
+    cy.get('[data-test="count"]')
+      .eq(2)
+      .should('have.text', '0')
 
     cy.get('#app').screenshot('initial-display')
   })
 
   it('Click the add-count, update the count', () => {
-    cy.get('[data-test="add-count"]').click()
+    cy.get('[data-test="add-count"]')
+      .eq(0)
+      .click()
 
-    cy.get('[data-test="count"]').should('have.text', '1')
-    cy.get('[data-test="axios-count"]').should('have.text', '0')
-    cy.get('[data-test="async-await-count"]').should('have.text', '0')
+    cy.get('[data-test="count"]')
+      .eq(0)
+      .should('have.text', '1')
+    cy.get('[data-test="count"]')
+      .eq(1)
+      .should('have.text', '0')
+    cy.get('[data-test="count"]')
+      .eq(2)
+      .should('have.text', '0')
 
     cy.get('#app').screenshot('add-count')
   })
 
-  it('Click the add-axios-count, update the axiosCount', () => {
-    cy.get('[data-test="add-axios-count"]').click()
+  it('Click the add-count for axios, update the count for axios', () => {
+    cy.get('[data-test="add-count"]')
+      .eq(1)
+      .click()
 
-    cy.get('[data-test="count"]').should('have.text', '0')
-    cy.get('[data-test="axios-count"]').should('have.text', '2')
-    cy.get('[data-test="async-await-count"]').should('have.text', '0')
+    cy.get('[data-test="count"]')
+      .eq(0)
+      .should('have.text', '0')
+    cy.get('[data-test="count"]')
+      .eq(1)
+      .should('have.text', '2')
+    cy.get('[data-test="count"]')
+      .eq(2)
+      .should('have.text', '0')
 
     cy.get('#app').screenshot('add-axios-count')
   })
 
-  it('Click the add-async-await-count, update the asyncAwaitCount', () => {
-    cy.get('[data-test="add-async-await-count"]').click()
+  it('Click the add-count for async/await, update the count for async/await', () => {
+    cy.get('[data-test="add-count"]')
+      .eq(2)
+      .click()
 
-    cy.get('[data-test="count"]').should('have.text', '0')
-    cy.get('[data-test="axios-count"]').should('have.text', '0')
-    cy.get('[data-test="async-await-count"]').should('have.text', '3')
+    cy.get('[data-test="count"]')
+      .eq(0)
+      .should('have.text', '0')
+    cy.get('[data-test="count"]')
+      .eq(1)
+      .should('have.text', '0')
+    cy.get('[data-test="count"]')
+      .eq(2)
+      .should('have.text', '3')
 
     cy.get('#app').screenshot('add-async-await-count')
   })
